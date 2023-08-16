5101305cde47a4e26c435cbb3073217c08689c1f
Peter-MJ-Parker <georgeneedham7@gmail.com>
2023-08-16 15:32:31 +0000

    feat/ecoSystem

--- src/Structures/schemas/money.ts
+++ src/Structures/schemas/money.ts
@@ -1,15 +1,15 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
 export default model(
-  "Money",
-  new Schema({
-    username: { type: String },
-    nickname: String,
-    userID: { type: String, require: true },
-    guildName: { type: String, require: true },
-    serverID: { type: String, require: true },
-    wallet: { type: Number, default: 0 },
-    bank: { type: Number, default: 0 }
-  }),
-  "Money"
-)
+	'Money',
+	new Schema({
+		username: { type: String },
+		nickname: String,
+		userID: { type: String, required: true },
+		guildName: { type: String, required: true },
+		serverID: { type: String, required: true },
+		wallet: { type: Number, default: 0 },
+		bank: { type: Number, default: 0 },
+	}),
+	'Money'
+);
