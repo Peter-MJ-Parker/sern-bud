7eba83b6aff4fba07ee6b53185d189982de78306
Peter-MJ-Parker <georgeneedham7@gmail.com>
2023-09-06 10:13:13 -0500

    refactor: change defaults to empty

--- src/Structures/schemas/guild.ts
+++ src/Structures/schemas/guild.ts
@@ -1,22 +1,22 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
 export default model(
-  "Guild",
-  new Schema({
-    gID: { type: String },
-    gName: { type: String },
-    prefix: { type: String, default: "?" },
-    modC: { type: String, default: "744945603313795306" },
-    verifiedRole: { type: String, default: "678400106982277130" },
-    welcomeC: { type: String, default: "678400867799400468" },
-    leaveC: { type: String, default: "744945603313795306" },
-    introC: { type: String, default: "744927896187043940" },
-    userCount: { type: Number },
-    botCount: { type: Number },
-    allCount: { type: Number },
-    userCountChan: { type: String, default: "825555223321640970" },
-    botCountChan: { type: String, default: "825555224147263549" },
-    allCountChan: { type: String, default: "825555222281060363" }
-  }),
-  "Guild"
-)
+	'Guild',
+	new Schema({
+		gID: { type: String },
+		gName: { type: String },
+		prefix: { type: String, default: '?' },
+		modC: { type: String, default: '' },
+		verifiedRole: { type: String, default: '' },
+		welcomeC: { type: String, default: '' },
+		leaveC: { type: String, default: '' },
+		introC: { type: String, default: '' },
+		userCount: { type: Number, default: 0 },
+		botCount: { type: Number, default: 0 },
+		allCount: { type: Number, default: 0 },
+		userCountChan: { type: String, default: '' },
+		botCountChan: { type: String, default: '' },
+		allCountChan: { type: String, default: '' },
+	}),
+	'Guild'
+);
