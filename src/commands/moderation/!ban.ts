156ffebd0fe397d73097b5b31e89ea5da6bb9751
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-07-28 07:53:12 -0500

    finalize

--- src/commands/moderation/!ban.ts
+++ src/commands/moderation/!ban.ts
@@ -4,5 +4,5 @@ export default commandModule({
   type: CommandType.Text,
   plugins: [],
   description: 'Manually bans a user before entering guild.',
-  execute: async ({ message }, tbd) => {}
+  execute: async (ctx, tbd) => {}
 });
