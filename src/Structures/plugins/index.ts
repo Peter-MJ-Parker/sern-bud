317b780794a8a1618aa231bf8b6e187cedddfc21
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-07-28 18:32:43 -0500

    add ownerOnly plugin

--- src/Structures/plugins/index.ts
+++ src/Structures/plugins/index.ts
@@ -1,3 +1,4 @@
 export * from './requirePermission.js';
 export * from './buttonConfirmation.js';
 export * from './confirmation.js';
+export * from './ownerOnly.js';
---
+++ src/Structures/plugins/ownerOnly.ts
@@ -0,0 +1,28 @@
+/**
+ * @plugin
+ * This is OwnerOnly plugin, it allows only bot owners to run the command, like eval.
+ *
+ * @author @EvolutionX-10 [<@697795666373640213>]
+ * @version 1.2.0
+ * @example
+ * ```ts
+ * import { ownerOnly } from "../plugins/ownerOnly";
+ * import { commandModule } from "@sern/handler";
+ * export default commandModule({
+ *  plugins: [ ownerOnly() ], // can also pass array of IDs to override default owner IDs
+ *  execute: (ctx) => {
+ * 		//your code here
+ *  }
+ * })
+ * ```
+ * @end
+ */
+
+import { CommandType, CommandControlPlugin, controller } from '@sern/handler';
+export function ownerOnly(override?: string[]) {
+  return CommandControlPlugin<CommandType.Both>((ctx, { deps }) => {
+    const { owners } = deps['@sern/client'].utils;
+    if ((override ?? owners).includes(ctx.user.id)) return controller.next();
+    return controller.stop();
+  });
+}
