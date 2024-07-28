1260ea3f9f602886a4713b046bea48c1f30ce62d
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-07-28 18:48:11 -0500

    feat: kill task by command

---
+++ src/commands/moderation/kill-task.ts
@@ -0,0 +1,41 @@
+import { buttonConfirmation, ownerOnly } from '#plugins';
+import { commandModule, CommandType } from '@sern/handler';
+import { publishConfig } from '@sern/publisher';
+import { ApplicationCommandOptionType, PermissionFlagsBits } from 'discord.js';
+
+export default commandModule({
+  type: CommandType.Slash,
+  description: 'Kills a scheduled task by uuid.',
+  plugins: [
+    publishConfig({
+      guildIds: ['678398938046267402'],
+      defaultMemberPermissions: [PermissionFlagsBits.Administrator]
+    }),
+    ownerOnly(),
+    buttonConfirmation()
+  ],
+  options: [
+    {
+      type: ApplicationCommandOptionType.String,
+      name: 'uuid',
+      description: 'Kills specified task.',
+      required: true,
+      autocomplete: true,
+      command: {
+        async execute(ctx, { deps }) {
+          const { tasks } = deps['@sern/scheduler'];
+          const focusedOption = ctx.options.getFocused().toLowerCase();
+          const filter = tasks.filter(f => f.toLowerCase().startsWith(focusedOption)).map(t => ({ name: t, value: t }));
+          await ctx.respond(filter);
+        }
+      }
+    }
+  ],
+  async execute(ctx, { deps }) {
+    const uuid = ctx.options.getString('uuid', true);
+    deps['@sern/scheduler'].kill(uuid);
+    return await ctx.interaction.editReply({
+      content: `Task: \`${uuid}\` has been stopped.`
+    });
+  }
+});
