c5ca254f447a413cbc15fffd9c2ae4c071a49b7a
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-07-28 17:16:17 -0500

    remove import

--- src/Structures/utils/adapters/Welcome/gif.ts
+++ src/Structures/utils/adapters/Welcome/gif.ts
@@ -1,117 +1,98 @@
 import { ButtonInteraction, GuildMember, TextChannel } from 'discord.js';
-import custom from '#schemas/customMessages';
 
 export const gifs = [
-	'https://i.imgur.com/f5DOAFz.gif',
-	'https://i.imgur.com/mdFUdn4.gif',
-	'https://i.imgur.com/CnQfH1w.gif',
-	'https://i.imgur.com/I7gHjTl.gif',
-	'https://i.imgur.com/fb5Tl3E.gif',
-	'https://i.imgur.com/8IBM9kY.gif',
-	'https://i.imgur.com/8yVGhcw.gif',
-	'https://i.imgur.com/EOqtlZs.gif',
-	'https://i.imgur.com/5S0KTnM.gif',
-	'https://i.imgur.com/zb5n4qP.gif',
-	'https://i.imgur.com/m074qMp.gif',
-	'https://i.imgur.com/Hl2qAoQ.gif',
-	'https://i.imgur.com/MIEV4L8.gif',
-	'https://i.imgur.com/k4kZShH.gif',
-	'https://i.imgur.com/aWyP4Lc.gif',
-	'https://i.imgur.com/uHiWMCB.gif',
-	'https://i.imgur.com/JL5ogD1.gif',
-	'https://i.imgur.com/PYdFdsf.gif',
-	'https://i.imgur.com/cVFf21A.gif',
-	'https://i.imgur.com/fWQukMs.gif',
-	'https://i.imgur.com/5oN1C8K.gif',
-	'https://i.imgur.com/gsoosza.gif',
-	'https://i.imgur.com/Z6qZMxj.gif',
-	'https://i.imgur.com/Tf6BE4N.gif',
-	'https://i.imgur.com/s6k4Y4v.gif',
-	'https://i.imgur.com/rUji7HE.gif',
-	'https://i.imgur.com/AhOWGfP.gif',
-	'https://i.imgur.com/mnFmDMl.gif',
-	'https://i.imgur.com/DZVEFlf.gif',
-	'https://i.imgur.com/5iNejET.gif',
-	'https://i.imgur.com/Uw2h9eG.gif',
-	'https://i.imgur.com/XkNbhds.gif',
-	'https://i.imgur.com/ppC3R0B.gif',
-	'https://i.imgur.com/wsyK0Yy.gif',
-	'https://i.imgur.com/cr6KL8b.gif',
-	'https://astra.ifunny.lol/8dT4ODrgQs.gif',
-	'https://i.imgur.com/IWL4w0g.gif',
-	'https://i.imgur.com/ONtZNzW.gif',
-	'https://i.imgur.com/4GcJFpT.gif',
-	'https://i.imgur.com/a8Mtb2s.gif',
-	'https://i.imgur.com/hyABHxi.gif',
-	'https://i.imgur.com/q1eBTCD.gif',
+  'https://i.imgur.com/f5DOAFz.gif',
+  'https://i.imgur.com/mdFUdn4.gif',
+  'https://i.imgur.com/CnQfH1w.gif',
+  'https://i.imgur.com/I7gHjTl.gif',
+  'https://i.imgur.com/fb5Tl3E.gif',
+  'https://i.imgur.com/8IBM9kY.gif',
+  'https://i.imgur.com/8yVGhcw.gif',
+  'https://i.imgur.com/EOqtlZs.gif',
+  'https://i.imgur.com/5S0KTnM.gif',
+  'https://i.imgur.com/zb5n4qP.gif',
+  'https://i.imgur.com/m074qMp.gif',
+  'https://i.imgur.com/Hl2qAoQ.gif',
+  'https://i.imgur.com/MIEV4L8.gif',
+  'https://i.imgur.com/k4kZShH.gif',
+  'https://i.imgur.com/aWyP4Lc.gif',
+  'https://i.imgur.com/uHiWMCB.gif',
+  'https://i.imgur.com/JL5ogD1.gif',
+  'https://i.imgur.com/PYdFdsf.gif',
+  'https://i.imgur.com/cVFf21A.gif',
+  'https://i.imgur.com/fWQukMs.gif',
+  'https://i.imgur.com/5oN1C8K.gif',
+  'https://i.imgur.com/gsoosza.gif',
+  'https://i.imgur.com/Z6qZMxj.gif',
+  'https://i.imgur.com/Tf6BE4N.gif',
+  'https://i.imgur.com/s6k4Y4v.gif',
+  'https://i.imgur.com/rUji7HE.gif',
+  'https://i.imgur.com/AhOWGfP.gif',
+  'https://i.imgur.com/mnFmDMl.gif',
+  'https://i.imgur.com/DZVEFlf.gif',
+  'https://i.imgur.com/5iNejET.gif',
+  'https://i.imgur.com/Uw2h9eG.gif',
+  'https://i.imgur.com/XkNbhds.gif',
+  'https://i.imgur.com/ppC3R0B.gif',
+  'https://i.imgur.com/wsyK0Yy.gif',
+  'https://i.imgur.com/cr6KL8b.gif',
+  'https://astra.ifunny.lol/8dT4ODrgQs.gif',
+  'https://i.imgur.com/IWL4w0g.gif',
+  'https://i.imgur.com/ONtZNzW.gif',
+  'https://i.imgur.com/4GcJFpT.gif',
+  'https://i.imgur.com/a8Mtb2s.gif',
+  'https://i.imgur.com/hyABHxi.gif',
+  'https://i.imgur.com/q1eBTCD.gif'
 ];
-export const welcomeEmojis = [
-	'💕',
-	'👌',
-	'🙌',
-	'<a:SHROOOOM:1179060921260322886>',
-];
+export const welcomeEmojis = ['💕', '👌', '🙌', '<a:SHROOOOM:1179060921260322886>'];
 export async function sticker(interaction: ButtonInteraction) {
-	let img = gifs[Math.floor(Math.random() * gifs.length) + 1];
-	let memberId = `${interaction.message.attachments
-		.first()
-		?.name?.split('-')[1]
-		.slice(0, -4)}`;
+  let img = gifs[Math.floor(Math.random() * gifs.length) + 1];
+  let memberId = `${interaction.message.attachments.first()?.name?.split('-')[1].slice(0, -4)}`;
 
-	let member = interaction.guild?.members.cache.get(memberId);
-	const contents = [
-		`:wave: Welcome to ${interaction.guild?.name}, ${member}`,
-		`Hi ${member}! Welcome to our community! Please make yourself at home!`,
-		`👋 Hello ${member}`,
-		`I'm glad you're here, ${member}! Please pass the joint!`,
-	];
-	let option = Math.floor(Math.random() * contents.length);
-	let hello = contents[option];
-	if (interaction.user.id === memberId) {
-		return await interaction.reply({
-			content: "You don't need to wave to yourself...",
-			ephemeral: true,
-		});
-	}
-	await webhookCreate(
-		interaction.channel as TextChannel,
-		interaction.member as GuildMember,
-		hello,
-		img
-	);
+  let member = interaction.guild?.members.cache.get(memberId);
+  const contents = [
+    `:wave: Welcome to ${interaction.guild?.name}, ${member}`,
+    `Hi ${member}! Welcome to our community! Please make yourself at home!`,
+    `👋 Hello ${member}`,
+    `I'm glad you're here, ${member}! Please pass the joint!`
+  ];
+  let option = Math.floor(Math.random() * contents.length);
+  let hello = contents[option];
+  if (interaction.user.id === memberId) {
+    return await interaction.reply({
+      content: "You don't need to wave to yourself...",
+      ephemeral: true
+    });
+  }
+  await webhookCreate(interaction.channel as TextChannel, interaction.member as GuildMember, hello, img);
 }
 
-export async function webhookCreate(
-	channel: TextChannel,
-	user: GuildMember,
-	msg: string,
-	file?: string
-) {
-	await channel
-		.createWebhook({
-			name: user.displayName,
-			avatar: user.displayAvatarURL(),
-		})
-		.then(async (s) => {
-			if (file) {
-				await s.send({
-					content: msg,
-					files: [file],
-					allowedMentions: {
-						users: [],
-					},
-				});
-			} else {
-				await s.send({
-					content: msg,
-					allowedMentions: {
-						users: [],
-					},
-				});
-			}
+export async function webhookCreate(channel: TextChannel, user: GuildMember, msg: string, file?: string) {
+  await channel
+    .createWebhook({
+      name: user.displayName,
+      avatar: user.displayAvatarURL()
+    })
+    .then(async s => {
+      if (file) {
+        await s.send({
+          content: msg,
+          files: [file],
+          allowedMentions: {
+            users: []
+          }
+        });
+      } else {
+        await s.send({
+          content: msg,
+          allowedMentions: {
+            users: []
+          }
+        });
+      }
 
-			setTimeout(async () => {
-				await s.delete();
-			}, 5000);
-		});
+      setTimeout(async () => {
+        await s.delete();
+      }, 5000);
+    });
 }
