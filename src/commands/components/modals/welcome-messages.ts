75881afd892d20d609596940b4c6c0db65049dc5
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-09-16 11:50:12 -0500

    notify user of switch between random and single

--- src/commands/components/modals/welcome-messages.ts
+++ src/commands/components/modals/welcome-messages.ts
@@ -8,37 +8,59 @@ export default commandModule({
   type: CommandType.Modal,
   async execute(ctx, { deps, params }) {
     if (!params) return;
-    const db = deps.prisma;
+    const db = deps.prisma.customWelcomeMessages;
 
     const memberId = ctx.user.id;
     const random = params === 'random';
 
     let updateData: CustomWelcomeMessagesUpdateInput = { random };
     let createData: CustomWelcomeMessagesCreateInput = { memberId, random };
-    let responseMessage: string;
+    let responseMessage: string = '';
 
-    if (params === 'one') {
-      const singleMessage = ctx.fields.getTextInputValue('custom_message');
-      updateData.singleMessage = singleMessage;
-      createData.singleMessage = singleMessage;
-      responseMessage = `I have saved your single message to reply to new users as:\n**${singleMessage}**`;
-    } else {
-      const [m1, m2, m3, m4, m5] = [
-        ctx.fields.getTextInputValue('custom_message_one'),
-        ctx.fields.getTextInputValue('custom_message_two'),
-        ctx.fields.getTextInputValue('custom_message_three'),
-        ctx.fields.getTextInputValue('custom_message_four'),
-        ctx.fields.getTextInputValue('custom_message_five')
-      ];
-      const messagesArray = [m1, m2, m3, m4, m5].filter(m => m !== '');
-      updateData.messagesArray = messagesArray;
-      createData.messagesArray = messagesArray;
-      responseMessage = `I have saved your multiple messages to reply to new users as:\n**${messagesArray.join(
-        '\n'
-      )}**`;
+    let _current = await db.findUnique({
+      where: {
+        memberId: ctx.user.id
+      }
+    });
+
+    switch (params) {
+      case 'one':
+        const singleMessage = ctx.fields.getTextInputValue('custom_message');
+        if (singleMessage === _current?.singleMessage) {
+          responseMessage = `No changes detected. Your current configuration is being used.`;
+          break;
+        }
+        updateData.singleMessage = singleMessage;
+        createData.singleMessage = singleMessage;
+        responseMessage = `I have saved your single message to reply to new users as:\n**${singleMessage}**`;
+        break;
+
+      case 'random':
+        const [m1, m2, m3, m4, m5] = [
+          ctx.fields.getTextInputValue('custom_message_one'),
+          ctx.fields.getTextInputValue('custom_message_two'),
+          ctx.fields.getTextInputValue('custom_message_three'),
+          ctx.fields.getTextInputValue('custom_message_four'),
+          ctx.fields.getTextInputValue('custom_message_five')
+        ];
+        const messagesArray = [m1, m2, m3, m4, m5].filter(m => m !== '');
+        if (messagesArray === _current?.messagesArray) {
+          responseMessage = `No changes detected. Your current configuration is being used.`;
+          break;
+        }
+        updateData.messagesArray = messagesArray;
+        createData.messagesArray = messagesArray;
+        responseMessage = `I have saved your multiple messages to reply to new users as:\n**${messagesArray.join(
+          '\n'
+        )}**`;
+        break;
     }
 
-    await db.customWelcomeMessages.upsert({
+    if (_current?.random !== random) {
+      responseMessage += `\nI have switched to using **${random ? 'random messages' : 'a single message'}** for you.`;
+    }
+
+    await db.upsert({
       where: { memberId },
       update: updateData,
       create: createData
