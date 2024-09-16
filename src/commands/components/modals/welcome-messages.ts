c1b41360da504e7c3e6c12b21abe0b1346b4db16
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-09-16 11:18:55 -0500

    don't edit existing set

--- src/commands/components/modals/welcome-messages.ts
+++ src/commands/components/modals/welcome-messages.ts
@@ -9,29 +9,20 @@ export default commandModule({
   async execute(ctx, { deps, params }) {
     if (!params) return;
     const db = deps.prisma;
+
     const memberId = ctx.user.id;
     const random = params === 'random';
 
-    let updateData: CustomWelcomeMessagesUpdateInput;
-    let createData: CustomWelcomeMessagesCreateInput;
+    let updateData: CustomWelcomeMessagesUpdateInput = { random };
+    let createData: CustomWelcomeMessagesCreateInput = { memberId, random };
     let responseMessage: string;
 
     if (params === 'one') {
-      const m = ctx.fields.getTextInputValue('custom_message');
-      updateData = {
-        random,
-        singleMessage: m,
-        messagesArray: []
-      };
-      createData = {
-        memberId,
-        random,
-        singleMessage: m,
-        messagesArray: []
-      };
-      responseMessage = `I have saved your message to reply to new users as:\n**${m}**`;
+      const singleMessage = ctx.fields.getTextInputValue('custom_message');
+      updateData.singleMessage = singleMessage;
+      createData.singleMessage = singleMessage;
+      responseMessage = `I have saved your single message to reply to new users as:\n**${singleMessage}**`;
     } else {
-      params = 'random';
       const [m1, m2, m3, m4, m5] = [
         ctx.fields.getTextInputValue('custom_message_one'),
         ctx.fields.getTextInputValue('custom_message_two'),
@@ -40,18 +31,11 @@ export default commandModule({
         ctx.fields.getTextInputValue('custom_message_five')
       ];
       const messagesArray = [m1, m2, m3, m4, m5].filter(m => m !== '');
-      updateData = {
-        random,
-        messagesArray,
-        singleMessage: null
-      };
-      createData = {
-        memberId,
-        random,
-        messagesArray,
-        singleMessage: null
-      };
-      responseMessage = `I have saved your messages to reply to new users as:\n**${messagesArray.join('\n')}**`;
+      updateData.messagesArray = messagesArray;
+      createData.messagesArray = messagesArray;
+      responseMessage = `I have saved your multiple messages to reply to new users as:\n**${messagesArray.join(
+        '\n'
+      )}**`;
     }
 
     await db.customWelcomeMessages.upsert({
