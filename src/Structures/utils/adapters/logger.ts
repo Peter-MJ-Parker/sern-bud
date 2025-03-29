aabc3344feb0a12c2417612499e703676bf718c2
Peter-MJ-Parker <georgeneedham7@gmail.com>
2025-03-29 05:52:19 -0500

    update: logger date string

--- src/Structures/utils/adapters/logger.ts
+++ src/Structures/utils/adapters/logger.ts
@@ -3,41 +3,35 @@ import { Logger, type LogLevel, type LogStyle } from '@spark.ts/logger';
 import { bold, italic } from 'colorette';
 
 export class Sparky implements Logging {
-	private _spark!: Logger;
-	private _date!: Date;
-	constructor(logLevel: LogLevel, logStyle: LogStyle) {
-		console.clear();
-		this._spark = new Logger({ logLevel, logStyle });
-		this._date = new Date();
-	}
+  private _spark!: Logger;
+  private _date!: Date;
+  constructor(logLevel: LogLevel, logStyle: LogStyle) {
+    console.clear();
+    this._spark = new Logger({ logLevel, logStyle });
+    this._date = new Date();
+  }
 
-	public warn = this.warning;
-	success(payload: LogPayload<unknown> | any): void {
-		payload = payload.message || { payload }.payload;
-		this._spark.success(
-			bold(italic(this._date.toISOString() + ' => ' + payload))
-		);
-	}
-	info(payload: LogPayload<unknown> | any): void {
-		payload = payload.message || { payload }.payload;
-		this._spark.info(bold(italic(this._date.toISOString() + ' => ' + payload)));
-	}
-	warning(payload: LogPayload<unknown> | any): void {
-		payload = payload.message || { payload }.payload;
-		this._spark.warn(bold(italic(this._date.toISOString() + ' => ' + payload)));
-	}
-	debug(payload: LogPayload<unknown> | any): void {
-		payload = payload.message || { payload }.payload;
-		this._spark.debug(
-			bold(italic(this._date.toISOString() + ' => ' + payload))
-		);
-	}
-	error(payload: LogPayload<unknown> | any): void {
-		payload = payload.message || { payload }.payload;
-		this._spark.error(
-			bold(italic(this._date.toISOString() + ' => ' + payload))
-		);
-	}
+  public warn = this.warning;
+  success(payload: LogPayload<unknown> | any): void {
+    payload = payload.message || { payload }.payload;
+    this._spark.success(bold(italic(this._date.toLocaleDateString() + ' => ' + payload)));
+  }
+  info(payload: LogPayload<unknown> | any): void {
+    payload = payload.message || { payload }.payload;
+    this._spark.info(bold(italic(this._date.toLocaleDateString() + ' => ' + payload)));
+  }
+  warning(payload: LogPayload<unknown> | any): void {
+    payload = payload.message || { payload }.payload;
+    this._spark.warn(bold(italic(this._date.toLocaleDateString() + ' => ' + payload)));
+  }
+  debug(payload: LogPayload<unknown> | any): void {
+    payload = payload.message || { payload }.payload;
+    this._spark.debug(bold(italic(this._date.toLocaleDateString() + ' => ' + payload)));
+  }
+  error(payload: LogPayload<unknown> | any): void {
+    payload = payload.message || { payload }.payload;
+    this._spark.error(bold(italic(this._date.toLocaleDateString() + ' => ' + payload)));
+  }
 }
 
 export const logger = new Sparky('debug', 'highlight');
