b1817bc0646ebe426ac602a9d1c9d5594f8fea99
Peter-MJ-Parker <georgeneedham7@gmail.com>
2025-04-20 21:07:52 -0500

    add randomizer command to pick a random user from reactions

---
+++ src/commands/util/randomizer.ts
@@ -0,0 +1,43 @@
+import { commandModule, CommandType } from "@sern/handler";
+import { ApplicationCommandOptionType, TextChannel } from "discord.js";
+
+export default commandModule({
+  description: "Pick a random user who reacted with :tada:",
+  type: CommandType.Slash,
+  options: [
+    {
+      name: "message-id",
+      description: "ID of the message to pick from",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+  ],
+  execute: async (interaction) => {
+    const messageId = interaction.options.getString("message-id", true);
+    const msg = await (interaction.channel as TextChannel).messages.fetch(
+      messageId
+    );
+    if (!msg)
+      return await interaction.reply({
+        content: `The message ID needs to be valid, I shouldn't have to explain you this :i_mean:`,
+        ephemeral: true,
+      });
+    else {
+      let reaction = msg.reactions.cache.get(":tada:");
+      let users = await reaction!.users.fetch();
+
+      let userids: string[] = [];
+
+      users.map(async (user) => {
+        userids.push(user.id);
+      });
+
+      const randomizer = Math.floor(Math.random() * userids.length);
+      let finnal = userids[randomizer];
+
+      await interaction.reply({
+        content: `🥁 Drum roll please 🥁 \n\n||<@${finnal}>||`,
+      });
+    }
+  },
+});
