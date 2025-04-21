3008ca4a3263da7d205ae71ea17b24c075c46907
Peter-MJ-Parker <georgeneedham7@gmail.com>
2025-04-20 23:16:25 -0500

    fix: update randomizer command to use correct context and message flags

--- src/commands/util/randomizer.ts
+++ src/commands/util/randomizer.ts
@@ -1,5 +1,5 @@
 import { commandModule, CommandType } from "@sern/handler";
-import { ApplicationCommandOptionType, TextChannel } from "discord.js";
+import { ApplicationCommandOptionType, MessageFlags, TextChannel } from "discord.js";
 
 export default commandModule({
   description: "Pick a random user who reacted with :tada:",
@@ -12,25 +12,28 @@ export default commandModule({
       required: true,
     },
   ],
-  execute: async (interaction) => {
-    const messageId = interaction.options.getString("message-id", true);
-    const msg = await (interaction.channel as TextChannel).messages.fetch(
+  execute: async (ctx, { deps }) => {
+    const client = deps["@sern/client"];
+    const messageId = ctx.options.getString("message-id", true);
+    const msg = await (ctx.channel as TextChannel).messages.fetch(
       messageId
     );
     if (!msg)
-      return await interaction.reply({
+      return await ctx.reply({
         content: `The message ID needs to be valid, I shouldn't have to explain you this :i_mean:`,
-        ephemeral: true,
+        flags: MessageFlags.Ephemeral
       });
     else {
-        console.log('message: ', msg);
       let reaction = msg.reactions.cache.find(r => r.emoji.name === "🎉");
       if (!reaction) {
-        return await interaction.reply({
+        return await ctx.reply({
           content: 'No reactions with 🎉 found on the message.',
-          ephemeral: true,
+          flags: MessageFlags.Ephemeral
         });
       }
+      await ctx.reply({
+        content: `<a:loading:1177281558563016806> Choosing a random person that reacted with emoji: 🎉.\n\nMessage: ${msg.url}\n\n🥁 Drum roll please 🥁 <a:loading:1177281558563016806>`,
+      });
       let users = await reaction.users.fetch();
 
       let userids: string[] = [];
@@ -40,11 +43,10 @@ export default commandModule({
       });
 
       const randomizer = Math.floor(Math.random() * userids.length);
-      let finnal = userids[randomizer];
-
-      await interaction.reply({
-        content: `🥁 Drum roll please 🥁 \n\n||<@${finnal}>||`,
-        ephemeral: true,
+      let final = userids[randomizer];
+      await client.utils.delay(3000)
+      await ctx.interaction.editReply({
+        content: `<@${final}> is the winner!\n\n-# This is currently just for fun. No prizes will be given out for this.`,
       });
     }
   },
