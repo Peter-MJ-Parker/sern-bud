95ea516388bd1fc44169c1a28975628945c53169
Peter-MJ-Parker <georgeneedham7@gmail.com>
2025-05-17 08:31:01 -0500

    refactor birthday command to use subcommands for better structure and clarity

--- src/commands/util/birthday.ts
+++ src/commands/util/birthday.ts
@@ -1,5 +1,5 @@
 import { commandModule, CommandType } from '@sern/handler';
-import { ApplicationCommandOptionType, GuildMember, PermissionFlagsBits } from 'discord.js';
+import { ApplicationCommandOptionType, GuildMember, PermissionFlagsBits, User } from 'discord.js';
 
 export default commandModule({
   type: CommandType.Slash,
@@ -7,55 +7,104 @@ export default commandModule({
   plugins: [],
   options: [
     {
-      type: ApplicationCommandOptionType.String,
-      name: 'action',
-      description: 'What would you like to do?',
-      required: true,
-      choices: [
-        { name: 'Set a birthday', value: 'set' },
-        { name: 'Change a birthday', value: 'edit' },
-        { name: 'Delete a birthday', value: 'delete' }
+      type: ApplicationCommandOptionType.Subcommand,
+      name: 'set',
+      description: 'Set a birthday.',
+      options: [
+        {
+          type: ApplicationCommandOptionType.User,
+          name: 'user-to-add',
+          description: 'Select the user to add manually. (Only for Admins)'
+        },
+        {
+          type: ApplicationCommandOptionType.String,
+          name: 'date',
+          description: 'Birthday in (MM/DD) format.',
+          required: true
+        }
       ]
     },
     {
-      type: ApplicationCommandOptionType.User,
-      name: 'user-to-add',
-      description: 'Select the user to add manually. (Only for Admins)'
+      type: ApplicationCommandOptionType.Subcommand,
+      name: 'edit',
+      description: 'Edit a birthday.',
+      options: [
+        {
+          type: ApplicationCommandOptionType.String,
+          name: 'user-to-edit',
+          description: 'Select the user to edit manually. (Only for Admins)',
+          autocomplete: true,
+          command: {
+            onEvent: [],
+            async execute(ctx, { deps }) {
+              if (!(ctx.member as GuildMember).permissions.has(PermissionFlagsBits.Administrator)) {
+                return [{ name: "You do not have permission to manage other users' birthdays.", value: 'non-admin' }];
+              }
+              const guildBirthday = await deps.prisma.birthday.findFirst({
+                where: {
+                  gID: ctx.guildId!
+                },
+                include: { birthdays: true }
+              });
+              if (!guildBirthday) return [{ name: 'No birthdays found', value: 'none' }];
+
+              return guildBirthday.birthdays.map(b => ({
+                name: `${b.nickname} (${b.date})`,
+                value: b.userID
+              }));
+            }
+          }
+        },
+        {
+          type: ApplicationCommandOptionType.String,
+          name: 'date',
+          description: 'Birthday in (MM/DD) format.',
+          required: true
+        }
+      ]
     },
     {
-      type: ApplicationCommandOptionType.String,
-      name: 'date',
-      description: 'Bday in (MM/DD) format.'
+      type: ApplicationCommandOptionType.Subcommand,
+      name: 'delete',
+      description: 'Delete a birthday.',
+      options: [
+        {
+          type: ApplicationCommandOptionType.String,
+          name: 'user-to-delete',
+          description: 'Select the user to delete manually. (Only for Admins)',
+          autocomplete: true,
+          command: {
+            onEvent: [],
+            async execute(ctx, { deps }) {
+              if (!(ctx.member as GuildMember).permissions.has(PermissionFlagsBits.Administrator)) {
+                return [{ name: "You do not have permission to manage other users' birthdays.", value: 'non-admin' }];
+              }
+              const guildBirthday = await deps.prisma.birthday.findFirst({
+                where: {
+                  gID: ctx.guildId!
+                },
+                include: { birthdays: true }
+              });
+              if (!guildBirthday) return [{ name: 'No birthdays found', value: 'none' }];
+
+              return guildBirthday.birthdays.map(b => ({
+                name: `${b.nickname} (${b.date})`,
+                value: b.userID
+              }));
+            }
+          }
+        }
+      ]
     }
   ],
   async execute(ctx, { deps }) {
-    const [i, [birthdayModel, guildModel]] = [deps['task-logger'], [deps.prisma.birthday, deps.prisma.guild]];
-    const guild = await guildModel.findFirst({
-      where: {
-        gID: ctx.guildId!
-      }
-    });
-    const action = ctx.options.getString('action', true);
-    const dateOption = ctx.options.getString('date');
-
-    if (!dateOption || !i.bdays.isValidDate(dateOption)) {
-      return await ctx.reply({
-        flags: 64,
-        content: 'Please provide a valid date in format: `MM/DD`'
-      });
-    }
-
-    const date = dateOption;
-
-    const userToAdd = ctx.options.getUser('user-to-add');
-    if (!(ctx.member as GuildMember).permissions.has(PermissionFlagsBits.Administrator) && userToAdd) {
-      return await ctx.reply({
-        flags: 64,
-        content: "You are missing permission: `Administrator`. You cannot manage other users' birthdays."
-      });
-    }
-
-    const u = userToAdd ?? ctx.user;
+    const [i, birthdayModel] = [deps['task-logger'], deps.prisma.birthday];
+    const sub = ctx.options.getSubcommand(true);
+    const members = await ctx.guild?.members.fetch()!;
+    let userToAdd = ctx.options.getUser('user-to-add', false);
+    let userToEdit = ctx.options.getString('user-to-edit', false);
+    let userToDelete = ctx.options.getString('user-to-delete', false);
+    let content = '';
 
     let guildBirthday = await birthdayModel.findFirst({
       where: {
@@ -72,16 +121,57 @@ export default commandModule({
         }
       });
     }
-    const userBirthday = guildBirthday.birthdays.find(b => b.userID === u.id);
+    const userBirthday = guildBirthday.birthdays.find(
+      b => b.userID === (userToAdd?.id || userToEdit || userToDelete || ctx.user.id)
+    );
 
-    let content = '';
+    const pronoun = (user: User) => (user.id === ctx.user.id ? 'your' : `${user}'s`);
 
-    switch (action) {
-      case 'edit':
+    const actions = {
+      set: async () => {
+        let date = ctx.options.getString('date', true);
+        if (!date || !i.bdays.isValidDate(date)) {
+          content = 'Please provide a date in format: `MM/DD`';
+        }
+        if (!userToAdd) {
+          userToAdd = ctx.user;
+        }
+        if (userBirthday) {
+          content = `I already have ${pronoun(userToAdd)} birthday saved in my memory as \`${userBirthday.date}\`.`;
+        } else {
+          await birthdayModel.update({
+            where: { id: guildBirthday.id },
+            data: {
+              birthdays: {
+                push: {
+                  nickname: userToAdd.displayName,
+                  username: userToAdd.username,
+                  date,
+                  userID: userToAdd.id
+                }
+              }
+            }
+          });
+          content = `I have set ${pronoun(userToAdd)} birthday as ${date}.`;
+        }
+        return { flags: 64, content };
+      },
+      edit: async () => {
+        if (userToEdit === 'none') {
+          content = 'No birthdays were found to edit.';
+        } else if (userToEdit === 'non-admin') {
+          content = "You do not have permission to manage other users' birthdays.";
+        }
+        let date = ctx.options.getString('date', true);
+        if (!date || !i.bdays.isValidDate(date)) {
+          content = 'Please provide a new date in format: `MM/DD`';
+        }
+        if (!userToEdit) {
+          userToEdit = ctx.user.id;
+        }
+        const { user: editableUser } = members.get(userToEdit)!;
         if (!userBirthday) {
-          content = `I do not have ${
-            u.id === ctx.user.id ? 'your' : `${userToAdd}'s`
-          } birthday saved in my memory. Please set it first.`;
+          content = `I do not have ${pronoun(editableUser)} birthday saved in my memory. Please set it first.`;
         } else if (userBirthday.date === date) {
           content = `That is the current date. Please use a different date.`;
         } else {
@@ -90,58 +180,56 @@ export default commandModule({
             data: {
               birthdays: {
                 updateMany: {
-                  where: { userID: u.id },
-                  data: { nickname: u.displayName, date }
+                  where: { userID: userToEdit },
+                  data: { nickname: editableUser.displayName, date }
                 }
               }
             }
           });
-          content = `I have updated ${u.id === ctx.user.id ? 'your' : `${userToAdd}'s`} birthday to \`${date}\`.`;
+          content = `I have updated ${pronoun(editableUser)} birthday to \`${date}\`.`;
         }
-        break;
+        return { flags: 64, content };
+      },
+      delete: async () => {
+        switch (userToDelete) {
+          case 'none':
+            content = 'No birthdays were found to delete.';
+            break;
+          case 'non-admin':
+            content = "You do not have permission to manage other users' birthdays.";
+            break;
+          default:
+            if (!userToDelete) {
+              userToDelete = ctx.user.id;
+            }
+            const { user: deletableUser } = members.get(userToDelete)!;
+            if (userBirthday) {
+              await birthdayModel.update({
+                where: { id: guildBirthday.id },
+                data: {
+                  birthdays: {
+                    deleteMany: { where: { userID: userToDelete } }
+                  }
+                }
+              });
+              content = `I have deleted ${pronoun(deletableUser)} birthday from my memory.`;
+            } else {
+              content = `I do not have ${pronoun(deletableUser)} birthday saved in my memory. Have you set it already?`;
+            }
+            break;
+        }
 
-      case 'set':
-        if (userBirthday) {
-          content = `I already have ${
-            u.id === ctx.user.id ? 'your' : `${userToAdd}'s`
-          } birthday saved in my memory as \`${userBirthday.date}\`.`;
-        } else {
-          await birthdayModel.update({
-            where: { id: guildBirthday.id },
-            data: {
-              birthdays: {
-                push: {
-                  nickname: u.displayName,
-                  username: u.username,
-                  date,
-                  userID: u.id
-                }
-              }
-            }
-          });
-          content = `I have set ${u.id === ctx.user.id ? 'your' : `${userToAdd}'s`} birthday as ${date}.`;
-        }
-        break;
-      case 'delete':
-        if (userBirthday) {
-          await birthdayModel.update({
-            where: { id: guildBirthday.id },
-            data: {
-              birthdays: {
-                deleteMany: { where: { userID: u.id } }
-              }
-            }
-          });
-          content = `I have deleted ${u.id === ctx.user.id ? 'your' : `${userToAdd}'s`} birthday from my memory.`;
-        } else {
-          content = `I do not have ${
-            u.id === ctx.user.id ? 'your' : `${userToAdd}'s`
-          } birthday saved in my memory. Have you set it already?`;
-          break;
-        }
+        return { flags: 64, content };
+      }
+    };
+
+    type SubKey = keyof typeof actions;
+
+    const result = await actions[sub as SubKey]();
+    if (result) {
+      await ctx.reply(result);
     }
 
-    await ctx.reply({ flags: 64, content });
     //integrate multiguild separation
     await i.bdays.logBirthdays(ctx.guild!);
   }
